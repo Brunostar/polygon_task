4a4703c1234ab5db8559a3d3932d5689d15d5757
Brunostar <tanjubrunostar0@gmail.com>
2022-04-06 13:58:14 +0100

    created an EquilateralTriangle class, set the constructor and implemented Its polygon methods, extending from Triangle class

---
+++ src/com/nasiatech/brunostar/impl/EquilateralTriangle.java
@@ -0,0 +1,22 @@
+package com.nasiatech.brunostar.impl;
+
+public class EquilateralTriangle extends Triangle{
+    double side;
+    double height;
+    double base = side;
+
+    public EquilateralTriangle(double side, double height) {
+        this.side = side;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0.5 * 3 * side;
+    }
+}
