4f04105d40c86f0ad83bb941fe81eb5ef97c2c96
Brunostar <tanjubrunostar0@gmail.com>
2022-04-06 14:07:55 +0100

    created a Square class, set the constructor and implemented Its polygon methods, extending from Quadrilateral class

---
+++ src/com/nasiatech/brunostar/impl/Square.java
@@ -0,0 +1,19 @@
+package com.nasiatech.brunostar.impl;
+
+public class Square extends Quadrilateral{
+    double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+}
