6bf94c5db3b7023215223b6b505b0a13a807cb8e
Brunostar <tanjubrunostar0@gmail.com>
2022-04-06 13:22:16 +0100

    set the constructor and implemented the polygon methods for the Hexagon class

--- src/com/nasiatech/brunostar/impl/Hexagon.java
+++ src/com/nasiatech/brunostar/impl/Hexagon.java
@@ -1,4 +1,23 @@
 package com.nasiatech.brunostar.impl;
 
-public class Hexagon {
+import com.nasiatech.brunostar.Polygon;
+
+import static java.lang.Math.sqrt;
+
+public class Hexagon implements Polygon {
+    double side;
+
+    public Hexagon(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return (3 * sqrt(3))/2 * this.side * this.side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 6 * this.side;
+    }
 }
