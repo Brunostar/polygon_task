b3dd27435ac916ac1c9cad125d95704dd5a3bbde
Brunostar <tanjubrunostar0@gmail.com>
2022-04-06 13:11:27 +0100

    set the constructor and implemented the polygon methods for the Octagon class

--- src/com/nasiatech/brunostar/impl/Octagon.java
+++ src/com/nasiatech/brunostar/impl/Octagon.java
@@ -1,4 +1,23 @@
 package com.nasiatech.brunostar.impl;
 
-public class Octagon {
+import com.nasiatech.brunostar.Polygon;
+
+public class Octagon implements Polygon {
+    double side;
+    double apothem;
+
+    public Octagon(double side, double apothem) {
+        this.side = side;
+        this.apothem = apothem;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * this.apothem * this.getPerimeter();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 5 * this.side;
+    }
 }
