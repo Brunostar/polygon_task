56d6c404a767d42500058d907cff230cd3b7ea8c
Brunostar <tanjubrunostar0@gmail.com>
2022-04-06 11:28:51 +0100

    set the constructor and implemented the polygon methods for the Pentagon class

--- src/com/nasiatech/brunostar/impl/Pentagon.java
+++ src/com/nasiatech/brunostar/impl/Pentagon.java
@@ -1,4 +1,23 @@
 package com.nasiatech.brunostar.impl;
 
-public class Pentagon {
+import com.nasiatech.brunostar.Polygon;
+
+public class Pentagon implements Polygon {
+    double side;
+    double apothem;
+
+    public Pentagon(double side, double apothem) {
+        this.side = side;
+        this.apothem = apothem;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * this.apothem * this.getPerimeter();
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 5 * this.side;
+    }
 }
